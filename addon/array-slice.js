1be0c932625dde6d60b79361ea7badcf0fef2dd7
j- <jh@live.com.au>
2015-02-06 15:22:00 +1100

    Implemented proper array slice logic

--- addon/array-slice.js
+++ addon/array-slice.js
@@ -1,17 +1,90 @@
 import Em from 'ember';
+var get = Em.get;
+var set = Em.set;
 
-var ArraySlice = Ember.ArrayProxy.extend({
-	offset: 0,
-	limit: Infinity,
-	length: function () {
-		var limit  = Math.max(this.get('limit'), 0);
-		var offset = Math.max(this.get('offset'), 0);
-		var right  = this.get('content.length') - offset;
-		return Math.min(limit, right);
-	}.property('offset', 'limit', 'content.length'),
-	objectAtContent: function (i) {
-		var offset = Math.max(this.get('offset'), 0);
-		return this.get('content').objectAt(i + offset);
+var DEFAULT_OFFSET = 0;
+var DEFAULT_LIMIT = Infinity;
+
+var ArraySlice = Em.ArrayProxy.extend({
+	offset: function (key, offset, old) {
+		// getter
+		if (arguments.length <= 1) {
+			// default value
+			return DEFAULT_OFFSET;
+		}
+		// do not allow negative offset
+		offset = Math.max(offset, 0);
+		// setter
+		var diff = offset - old || 0;
+		// no work to do
+		if (diff === 0) {
+			return offset;
+		}
+		var content = get(this, 'content');
+		var limit = get(this, 'limit');
+		var arranged = get(this, 'arrangedContent');
+		var arrangedLength = get(arranged, 'length');
+		diff = Math.min(diff, limit);
+		// unshift
+		arranged.replace(
+			// beginning of array
+			0,
+			// remove count
+			Math.min(diff, arrangedLength),
+			// items to add
+			content.slice(offset, offset - diff)
+		);
+		// pop
+		var index = Math.min(limit, arrangedLength - diff);
+		index = Math.max(index, 0);
+		arranged.replace(
+			// end of array
+			index,
+			// remove count
+			get(arranged, 'length') - index,
+			// items to add
+			content.slice(offset + limit - diff, offset + limit)
+		);
+		return offset;
+	}.property(),
+
+	limit: function (key, limit, old) {
+		// getter
+		if (arguments.length <= 1) {
+			// default value
+			return DEFAULT_LIMIT;
+		}
+		// do not allow negative limit
+		limit = Math.max(limit, 0);
+		// setter
+		var offset = get(this, 'offset');
+		var content = get(this, 'content');
+		var arrangedContent = get(this, 'arrangedContent');
+		var arrangedLength = get(arrangedContent, 'length');
+		var diff = limit - arrangedLength;
+		var index = arrangedLength + offset;
+		var addedItems = content.slice(index, index + Math.max(diff, 0));
+		diff = Math.max(-diff, 0);
+		arrangedContent.replace(arrangedLength - diff, diff, addedItems);
+		return limit;
+	}.property(),
+
+	arrangedContent: function () {
+		var content = get(this, 'content');
+		var offset = get(this, 'offset');
+		var limit = get(this, 'limit');
+		var slice = content.slice(offset, offset + limit);
+		return slice;
+	}.property('content'),
+
+	contentArrayDidChange: function (content, i, removedCount, addedCount) {
+		var offset = get(this, 'offset');
+		var limit = get(this, 'limit');
+		var output = get(this, 'arrangedContent');
+		var length = get(output, 'length');
+		var addedItems = content.slice(offset, offset + addedCount);
+		output.replace(length - i - addedCount, addedCount);
+		output.replace(0, 0, addedItems);
 	}
 });
 
