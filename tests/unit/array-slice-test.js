7625791ba6b54b7805f395e12bf957e0fae42602
j- <jh@live.com.au>
2015-02-07 12:18:36 +1100

    Added basic unit test

--- tests/unit/array-slice-test.js
+++ tests/unit/array-slice-test.js
@@ -32,3 +32,44 @@ test('offset can be initialized', function () {
 	});
 	equal(slice.get('offset'), 2);
 });
+
+test('arranged content matches original content', function () {
+	var slice = ArraySlice.create({
+		content: ['a', 'b', 'c'],
+		offset: 2
+	});
+	equal(slice.objectAt(0), 'c');
+});
+
+test('arranged content length does not exceed limit', function () {
+	var slice = ArraySlice.create({
+		content: ['a', 'b', 'c'],
+		limit: 2
+	});
+	equal(slice.get('length'), 2);
+});
+
+test('offset can be updated', function () {
+	var slice = ArraySlice.create({
+		content: ['a', 'b', 'c']
+	});
+	equal(slice.objectAt(0), 'a');
+	equal(slice.get('length'), 3);
+	slice.set('offset', 1);
+	equal(slice.objectAt(0), 'b');
+	equal(slice.get('length'), 2);
+	slice.set('offset', 2);
+	equal(slice.objectAt(0), 'c');
+	equal(slice.get('length'), 1);
+});
+
+test('content array unshift', function () {
+	var arr = ['a', 'b', 'c'];
+	var slice = ArraySlice.create({
+		content: arr,
+		offset: 1
+	});
+	equal(slice.objectAt(0), 'b');
+	arr.unshiftObject('x');
+	equal(slice.objectAt(0), 'a');
+});
