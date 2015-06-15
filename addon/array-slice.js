352cbfa05ba4b5e0557e7363763978322f219003
j- <jh@live.com.au>
2015-06-15 13:48:13 +1000

    Separated logic into ArrayLimit and ArrayOffset proxies

--- addon/array-slice.js
+++ addon/array-slice.js
@@ -1,105 +1,62 @@
 import Em from 'ember';
-var get = Em.get;
-var set = Em.set;
+import ArrayLimit from 'array-limit';
+import ArrayOffset from 'array-offset';
+
+var computed = Em.computed;
+var alias = computed.alias;
 
 var DEFAULT_OFFSET = 0;
 var DEFAULT_LIMIT = Infinity;
 
 var ArraySlice = Em.ArrayProxy.extend({
-	offset: function (key, offset, old) {
-		// getter
-		if (arguments.length <= 1) {
-			// default value
+	offset: computed({
+		get: function () {
 			return DEFAULT_OFFSET;
+		},
+		set: function (key, offset) {
+			this.set('_offsetProxy.offset', offset);
+			return this.get('_offsetProxy.offset');
 		}
-		// do not allow negative offset
-		offset = Math.max(offset, 0);
-		// setter
-		var diff = offset - old || 0;
-		// no work to do
-		if (diff === 0) {
-			return offset;
-		}
-		var content = get(this, 'content');
-		var limit = get(this, 'limit');
-		var arranged = get(this, 'arrangedContent');
-		var arrangedLength = get(arranged, 'length');
-		diff = Math.min(diff, limit);
-		// unshift
-		arranged.replace(
-			// beginning of array
-			0,
-			// remove count
-			Math.min(diff, arrangedLength),
-			// items to add
-			content.slice(offset, offset - diff)
-		);
-		// pop
-		var index = Math.min(limit, arrangedLength - diff);
-		index = Math.max(index, 0);
-		arranged.replace(
-			// end of array
-			index,
-			// remove count
-			get(arranged, 'length') - index,
-			// items to add
-			content.slice(offset + limit - diff, offset + limit)
-		);
-		return offset;
-	}.property(),
+	}),
 
-	limit: function (key, limit, old) {
-		// getter
-		if (arguments.length <= 1) {
-			// default value
+	limit: computed({
+		get: function () {
 			return DEFAULT_LIMIT;
+		},
+		set: function (key, limit) {
+			this.set('_limitProxy.limit', limit);
+			return this.get('_limitProxy.limit');
 		}
-		// do not allow negative limit
-		limit = Math.max(limit, 0);
-		// setter
-		var offset = get(this, 'offset');
-		var content = get(this, 'content');
-		var arrangedContent = get(this, 'arrangedContent');
-		var arrangedLength = get(arrangedContent, 'length');
-		var diff = limit - arrangedLength;
-		var index = arrangedLength + offset;
-		var addedItems = content.slice(index, index + Math.max(diff, 0));
-		diff = Math.max(-diff, 0);
-		arrangedContent.replace(arrangedLength - diff, diff, addedItems);
-		return limit;
-	}.property(),
+	}),
 
-	arrangedContent: function () {
-		var content = get(this, 'content');
-		var offset = get(this, 'offset');
-		var limit = get(this, 'limit');
-		var slice = content.slice(offset, offset + limit);
-		return slice;
-	}.property('content'),
+	content: null,
 
-	contentArrayDidChange: function (content, i, removedCount, addedCount) {
-		var offset = get(this, 'offset');
-		var limit = get(this, 'limit');
-		var arranged = get(this, 'arrangedContent');
-		var arrangedLength = get(arranged, 'length');
-		var addedItems = content.slice(offset, offset + addedCount);
-		arranged.replace(
-			Math.max(0, arrangedLength - i - addedCount + offset),
-			addedCount
-		);
-		arranged.replace(0, 0, addedItems);
-	}
+	_offsetProxy: computed(function () {
+		return ArrayOffset.create({
+			content: this.get('content'),
+			offset: this.get('offset')
+		});
+	}),
+
+	_limitProxy: computed(function () {
+		return ArrayLimit.create({
+			content: this.get('_offsetProxy.arrangedContent'),
+			limit: this.get('limit')
+		});
+	}),
+
+	arrangedContent: alias('_limitProxy')
 });
 
 ArraySlice.computed = {
 	slice: function (prop, offset, limit) {
-		return function () {
+		return computed(prop, function () {
 			return ArraySlice.create({
 				content: this.get(prop),
 				offset: offset,
 				limit: limit
 			});
-		}.property(prop);
+		});
 	}
 };
 
